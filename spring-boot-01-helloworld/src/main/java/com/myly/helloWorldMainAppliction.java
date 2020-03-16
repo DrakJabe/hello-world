74450fc5a6c99a920f7816351079ff385ab6368e
DarkJabe <1210767303@qq.com>
2020-03-16 22:15:59 +0800

    First Commit

---
+++ spring-boot-01-helloworld/src/main/java/com/myly/helloWorldMainAppliction.java
@@ -0,0 +1,13 @@
+package com.myly;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class helloWorldMainAppliction {
+    public static void main(String[] args) {
+        //让主程序跑起来
+        SpringApplication.run(helloWorldMainAppliction.class,args);
+    }
+}
